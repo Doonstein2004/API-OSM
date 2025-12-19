60e4d6828ecb700391bfd08664e1614b946129c7
Daniel Bello <doonstein@gmail.com>
2025-12-19 17:17:31 -0300

    Adjust time to login

--- utils.py
+++ utils.py
@@ -118,7 +118,7 @@ def login_to_osm(page: Page, osm_username: str, osm_password: str, max_retries: 
                         print("  - [ACCIÓN] Enviando formulario con tecla ENTER...")
                         password_input.press("Enter")
                         
-                        time.sleep(3)
+                        time.sleep(8)
                         
                         # Esperamos a ver qué pasa (Navegación o Error)
                         try:
